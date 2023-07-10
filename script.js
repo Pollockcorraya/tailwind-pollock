4abf8b744caf4aaf32cd853789e13c04fa2910f8
Pollockcorraya <pollockcorraya@gmail.com>
2023-07-10 22:41:34 +0600

    tailwind

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.card-item').slick({
+        autoplay: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: false,
+        nextArrow: false,
+    });
+})
